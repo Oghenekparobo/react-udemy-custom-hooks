4e75e673c72933d1e8bd20f792392d6cf55949d8
Oghenekparobo <classicsteph11@gmail.com>
2022-11-22 10:26:51 +0100

    commit another example of custom hooks in react udemy curse using http request

---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,37 @@
+import useHttp from "../../hook/use-http";
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+const NewTask = (props) => {
+ 
+
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const enterTaskHandler = async (taskText) => {
+
+    const createTask = (taskText , taskData) => {
+      const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
+      props.onAddTask(createdTask);
+    };
+
+    sendTaskRequest({
+      url: "https://react-8f26a-default-rtdb.firebaseio.com/tasks.json",
+      method: "POST",
+      body: JSON.stringify({ text: taskText }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+   
+    } ,createTask.bind(null , taskText  ));
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
